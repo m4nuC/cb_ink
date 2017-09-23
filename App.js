f91a7574b7a84b1a6327a41de868f03480334f63
Emmanuel Chappat <0mbre@Emmanuels-MacBook-Pro.local>
2017-09-23 11:24:41 +0800

    Add react-native-pinch-zoom-view
    This was to test the behaviour of that module. Seems to work OK.

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PinchZoomView from 'react-native-pinch-zoom-view';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <PinchZoomView style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      </PinchZoomView>
     );
   }
 }
@@ -16,7 +17,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
