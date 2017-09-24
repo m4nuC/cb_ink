6340dbe06ab6664b6dcf5cf2e22c84bd9396925c
Emmanuel Chappat <m4nu@hotmail.fr>
2017-09-24 17:20:43 +0800

    Update QR code

--- utilities/constants.js
+++ utilities/constants.js
@@ -1,3 +1,3 @@
-export const TEARDROP_RADIUS = 14;
-export const ANGLELINE_BASE_RADIUS = 6;
+export const TEARDROP_RADIUS = 16;
+export const ANGLELINE_BASE_RADIUS = 10;
 export const MAIN_COLOR = '#8AEBFF';
